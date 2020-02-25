10bf6fc57a45db22540cd9a1c0b6a56c1b09140b
Bryce St. Pierre <brycelstpierre@gmail.com>
2020-02-24 23:27:46 -0500

    Updated HTML layout.

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 
 $(() => {
-  
+  var data = { "employee":{ "name":"John", "age":30, "city":"New York" }, "employee":{ "name":"John", "age":30, "city":"New York" } };
+  $('pre').text(JSON.stringify(data, null, 2));
+
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 });
