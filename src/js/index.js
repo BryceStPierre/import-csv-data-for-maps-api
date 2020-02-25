c997c6e6b861c5150aba545a0806b313935ea6e8
Bryce St. Pierre <brycelstpierre@gmail.com>
2020-02-24 20:41:27 -0500

    Installed bootstrap and updated initial CSS/JS.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-import '../style.css';
-
+import 'bootstrap';
 import $ from 'jquery';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.css';
 
 $(() => {
   alert('Hello world!');
