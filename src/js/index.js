27db4deb4e7d40faf6bcfa1c746fdc721216964b
Bryce St. Pierre <brycelstpierre@gmail.com>
2020-02-26 00:19:35 -0500

    Added more layout components and updated styling.

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,25 @@
-import 'bootstrap';
 import $ from 'jquery';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 
 $(() => {
+
+  $('#fieldsForm').submit(e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    $('#fieldsForm').addClass('was-validated');
+  });
+
   var data = { "employee":{ "name":"John", "age":30, "city":"New York" }, "employee":{ "name":"John", "age":30, "city":"New York" } };
   $('pre').text(JSON.stringify(data, null, 2));
 
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+    document.getElementById('map'), 
+    {
+      zoom: 4, 
+      center: {lat: -25.344, lng: 131.036}
+    }
+  );
 });
