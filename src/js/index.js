bb7d445e15c8541ef2c4e97460e7386bd4cb9fe7
Bryce St. Pierre <brycelstpierre@gmail.com>
2020-02-26 19:11:47 -0500

    Added layout elements, commented out unnecessary code.

--- src/js/index.js
+++ src/js/index.js
@@ -9,17 +9,19 @@ $(() => {
     e.preventDefault();
     e.stopPropagation();
 
-    $('#fieldsForm').addClass('was-validated');
+    if ($('#fieldsForm').get(0).checkValidity() === false)
+      $('#fieldsForm').addClass('was-validated');
+    
   });
 
-  var data = { "employee":{ "name":"John", "age":30, "city":"New York" }, "employee":{ "name":"John", "age":30, "city":"New York" } };
-  $('pre').text(JSON.stringify(data, null, 2));
+  // var data = { "employee":{ "name":"John", "age":30, "city":"New York" }, "employee":{ "name":"John", "age":30, "city":"New York" } };
+  // $('pre').text(JSON.stringify(data, null, 2));
 
-  var map = new google.maps.Map(
-    document.getElementById('map'), 
-    {
-      zoom: 4, 
-      center: {lat: -25.344, lng: 131.036}
-    }
-  );
+  // var map = new google.maps.Map(
+  //   document.getElementById('map'), 
+  //   {
+  //     zoom: 4, 
+  //     center: {lat: -25.344, lng: 131.036}
+  //   }
+  // );
 });
