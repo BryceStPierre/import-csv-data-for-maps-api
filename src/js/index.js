ea1902ce5cea06a53a2ad6c9deb6c09bf7b245aa
Bryce St. Pierre <brycelstpierre@gmail.com>
2020-02-26 22:57:48 -0500

    Added home and import section interactions.

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,68 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 
+import {
+  goToImportSection,
+  goToFieldsSection, 
+  goToRouteSection,
+  returnToImportSection,
+  returnToFieldsSection
+} from './interactions/navigation';
+
+import { store } from './utils/storage';
+
+import { handleFileInput } from './interactions/file';
+
+import { populateFields } from './interactions/fields';
+
 $(() => {
 
-  $('#fieldsForm').submit(e => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if ($('#fieldsForm').get(0).checkValidity() === false)
-      $('#fieldsForm').addClass('was-validated');
-    
+  /*
+   * Home Section
+   */
+  $('#getStartedButton').click(() => {
+    goToImportSection();
+    $('#importButton').prop('disabled', true);
   });
 
-  // var data = { "employee":{ "name":"John", "age":30, "city":"New York" }, "employee":{ "name":"John", "age":30, "city":"New York" } };
+  /*
+   * Import Section
+   */
+  $('#fileInput').change(e => {
+    handleFileInput(e, (err, imported) => {
+      store('fields', imported.fields);
+      store('data', imported.data);
+      $('#importButton').prop('disabled', false);
+    });
+  });
+
+  $('#importButton').click(() => {
+    if ($('#importForm').get(0).checkValidity() === false) {
+      $('#importForm').addClass('was-validated');
+    } else {
+      goToFieldsSection();
+      populateFields();
+    }
+  });
+
+  /*
+   * Fields Section
+   */
+  $('#importReturnButton').click(returnToImportSection);
+
+  $('#plotButton').click(() => {
+    if ($('#fieldsForm').get(0).checkValidity() === false)
+      $('#fieldsForm').addClass('was-validated');
+    else
+      goToRouteSection();
+  })
+
+  /*
+   * Route Section
+   */
+  $('#fieldsReturnButton').click(returnToFieldsSection);
+
+
   // $('pre').text(JSON.stringify(data, null, 2));
 
   // var map = new google.maps.Map(
@@ -24,4 +74,13 @@ $(() => {
   //     center: {lat: -25.344, lng: 131.036}
   //   }
   // );
+
+  $('#importForm').submit(e => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  $('#fieldsForm').submit(e => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
 });
